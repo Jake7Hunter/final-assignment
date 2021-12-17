afd5df20deaad73cff6ed487d3ce727db4b68cdf
Jake Hunter <jake7hunter@hmail.com>
2021-12-17 18:49:51 -0500

    changed song length to seconds & started inital filters

--- scripts.js
+++ scripts.js
@@ -1,20 +1,19 @@
 //Song playlist objects
-
 const allSongs = [
     {
       songId: 1,
       image: `adele.jpeg`,
       name: `Love Is A Game`,
       artist: `Adele`,
-      length: `6:30`,
+      length: 378,
       audio: `love-is-a-game.mp3`,
-      playing: false,
+      playing: true,
     }, {
         songId: 2,
         image: `soho-2.jpeg`,
         name: `You're My World (Soho)`,
         artist: `Anya Taylor-Joy`,
-        length: `2:18`,
+        length: 130,
         audio: `you-are-my-world-soho.mp3`,
         playing: false,
       }, {
@@ -22,7 +21,7 @@ const allSongs = [
         image: `everybodys-gotta-live.jpeg`,
         name: `Everybody's Gotta Live`,
         artist: `Love`,
-        length: `3:25`,
+        length: 195,
         audio: `everybodys-gotta-live.mp3`,
         playing: false,
       }, {
@@ -30,7 +29,7 @@ const allSongs = [
         image: `warning.jpg`,
         name: `Misery`,
         artist: `Green Day`,
-        length: `5:05`,
+        length: 303,
         audio: `misery.mp3`,
         playing: false,
       }, {
@@ -38,7 +37,7 @@ const allSongs = [
         image: `soho-3.jpg`,
         name: `Downtown (Uptempo)`,
         artist: `Anya Taylor-Joy`,
-        length: `3:30`,
+        length: 198,
         audio: `downtown-uptempo.mp3`,
         playing: false,
       }, {
@@ -46,7 +45,7 @@ const allSongs = [
         image: `land-of-1000-dances.jpg`,
         name: `Land Of 1000 Dances`,
         artist: `The Walker Brothers`,
-        length: `2:35`,
+        length: 141,
         audio: `land-of-1000-dances.mp3`,
         playing: false,
       }, {
@@ -54,7 +53,7 @@ const allSongs = [
         image: `soho-3.jpg`,
         name: `You're My World`,
         artist: `Anya Taylor Joy`,
-        length: `3:02`,
+        length: 181,
         audio: `everybodys-gotta-live.mp3`,
         playing: false,
       }, {
@@ -62,7 +61,7 @@ const allSongs = [
         image: `heroes.jpg`,
         name: `Heroes`,
         artist: `David Bowie`,
-        length: `3:36`,
+        length: 201,
         audio: `heroes.mp3`,
         playing: false,
     }, {
@@ -70,7 +69,7 @@ const allSongs = [
         image: `soho-2.jpeg`,
         name: `Neon`,
         artist: `Stephen Price`,
-        length: `5:09`,
+        length: 305,
         audio: `neon.mp3`,
         playing: false,
     }, {
@@ -78,35 +77,42 @@ const allSongs = [
         image: `soho-3.jpg`,
         name: `Downtown (Downtempo)`,
         artist: `Anya Taylor-Joy`,
-        length: `5:35`,
+        length: 321,
         audio: `downtown-downtempo.mp3`,
         playing: false,
-    }
-      
+    } 
 ]
     // Append a new list item with the Object data passed to the function
     const appendToList = function(item) {
         document.querySelector(`#playlist`).innerHTML += `
-        <article class="song">
+        <article class="song ${(item.playing) ? `playing` : ""}">
             <img src="./assets/covers/${item.image}" alt="${item.name}">
             <h2>${item.name}</h2>
             <h3>${item.artist}</h3>
-            <h3>${item.length}</h3>
-            <button id="playButton">Play</button>
+            <h3>${(item.length/60).toFixed(2)} mins</h3>
+            <button id="playButton${item.songId}">Play</button>
         </article>
       `
     }
+
+//Filter songs
+const maxLength = 200
+const perPage = 3
+
+//Filter each song
+allSongs.filter(item => item.length > 200)
+//Appends each object to HTML
+.forEach(appendToList)
+
+
     
-    //Appends each object to HTML
-    allSongs.forEach(appendToList)
+    
+  //   const song = new Audio()
 
-
-    // const playButton = document.querySelector(`#playButton`)
-    // const song = new Audio(`./assets/music/you-are-my-world-soho.mp3`)
-
-    // playButton.addEventListener(`click`, function(event) {
-    //     console.log(`playing`)
-    //     song.play()  
-    // })
+  //   playButton.addEventListener(`click`, function(event) {
+  //     console.log(`playing`)
+  //     song.play()  
+  // })
+    
 
     
