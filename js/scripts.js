c35ae2919cdfd7df3b215a79dd9f95ed210f8183
Jake Hunter <jake7hunter@hmail.com>
2021-12-18 16:12:11 -0500

    Assigned allSongs data to alternate js file & included "now playing" info

--- js/scripts.js
+++ js/scripts.js
@@ -1,87 +1,20 @@
-//Song playlist objects
-const allSongs = [
-    {
-      songId: 1,
-      image: `adele.jpeg`,
-      name: `Love Is A Game`,
-      artist: `Adele`,
-      length: 378,
-      audio: `love-is-a-game.mp3`,
-      playing: false,
-    }, {
-        songId: 2,
-        image: `soho-2.jpeg`,
-        name: `You're My World (Soho)`,
-        artist: `Anya Taylor-Joy`,
-        length: 130,
-        audio: `you-are-my-world-soho.mp3`,
-        playing: false,
-      }, {
-        songId: 3,
-        image: `everybodys-gotta-live.jpeg`,
-        name: `Everybody's Gotta Live`,
-        artist: `Love`,
-        length: 195,
-        audio: `everybodys-gotta-live.mp3`,
-        playing: false,
-      }, {
-        songId: 4,
-        image: `warning.jpg`,
-        name: `Misery`,
-        artist: `Green Day`,
-        length: 303,
-        audio: `misery.mp3`,
-        playing: false,
-      }, {
-        songId: 5,
-        image: `soho-3.jpg`,
-        name: `Downtown (Uptempo)`,
-        artist: `Anya Taylor-Joy`,
-        length: 198,
-        audio: `downtown-uptempo.mp3`,
-        playing: false,
-      }, {
-        songId: 6,
-        image: `land-of-1000-dances.jpg`,
-        name: `Land Of 1000 Dances`,
-        artist: `The Walker Brothers`,
-        length: 141,
-        audio: `land-of-1000-dances.mp3`,
-        playing: false,
-      }, {
-        songId: 7,
-        image: `soho-3.jpg`,
-        name: `You're My World`,
-        artist: `Anya Taylor-Joy`,
-        length: 181,
-        audio: `you-are-my-world-regular.mp3`,
-        playing: false,
-      }, {
-        songId: 8,
-        image: `heroes.jpg`,
-        name: `Heroes`,
-        artist: `David Bowie`,
-        length: 201,
-        audio: `heroes.mp3`,
-        playing: false,
-    }, {
-        songId: 9,
-        image: `soho-2.jpeg`,
-        name: `Neon`,
-        artist: `Stephen Price`,
-        length: 305,
-        audio: `neon.mp3`,
-        playing: false,
-    }, {
-        songId: 10,
-        image: `soho-3.jpg`,
-        name: `Downtown (Downtempo)`,
-        artist: `Anya Taylor-Joy`,
-        length: 321,
-        audio: `downtown-downtempo.mp3`,
-        playing: false,
-    } 
-]
+import { allSongs } from "./data.js"
+
+//Main variables for player
+let playingIndex = 0
+const song = new Audio()
+song.src = `/assets/music/${allSongs[playingIndex].audio}`
+const playButton = document.querySelector(`#playSong`)
+const playPrevious = document.querySelector(`#previousSong`)
+const playNext = document.querySelector(`#nextSong`)
+const playlistEle = document.querySelector(`#playlist`)
+const currentSong = document.querySelector(`#currentSong`)
+const currentSongInfo = function(){
+  currentSong.innerHTML = `<h2 class"currentSong">Current Song: ${allSongs[playingIndex].name}</h2> <h3 class="artist">By: ${allSongs[playingIndex].artist}</h3>`
+}
+//Filter Form shortcut
+const filterForm = document.querySelector(`#filter`)
+
 // Append a new list item with the Object data passed to the function
 const appendToList = function(item, index) {
     document.querySelector(`#playlist`).innerHTML += `
@@ -93,9 +26,6 @@ const appendToList = function(item, index) {
     </article>
   `
 }
-
-//Filter Form shortcut
-const filterForm = document.querySelector(`#filter`)
 
 filterForm.addEventListener(`input`, function(event) {
   //Stop for from refreshing
@@ -127,21 +57,8 @@ window.addEventListener(`load`, function(event) {
   filterAndPrint()
 });
 
-//Music Player
-
-//Main variables for player
-let playingIndex = 0
-const song = new Audio()
-song.src = `/assets/music/${allSongs[playingIndex].audio}`
-const playButton = document.querySelector(`#playSong`)
-const playPrevious = document.querySelector(`#previousSong`)
-const playNext = document.querySelector(`#nextSong`)
-const playlistEle = document.querySelector(`#playlist`)
-
 //Click play/pause button to start/stop song
 playButton.addEventListener(`click`, function(event){
-  isSongPlaying = true
-  console.log(isSongPlaying)
   if (song.paused){
     song.play();
     playButton.textContent = `⏸`
@@ -149,6 +66,8 @@ playButton.addEventListener(`click`, function(event){
     song.pause()
     playButton.textContent = `▶️`
   }
+  //Display current song info
+  currentSongInfo()
 })
 
 //Click "next" button to go to the next song 
@@ -161,6 +80,8 @@ playNext.addEventListener(`click`, function(event){
   } else {
     song.src = `/assets/music/${allSongs[playingIndex].audio}`
   }
+  //Display current song info
+  currentSongInfo()
 })
 
 //Click "previous" button to go to last song
@@ -173,6 +94,8 @@ playPrevious.addEventListener(`click`, function(event){
   } else {
     song.src = `/assets/music/${allSongs[playingIndex].audio}`
   }
+  //Display current song info
+  currentSongInfo()
 })
 
 //Click individual songs to have them play
@@ -183,7 +106,9 @@ playlistEle.addEventListener(`click`, function(event){
     playingIndex = Number(songToPlay.dataset.index)
     song.src = `/assets/music/${allSongs[playingIndex].audio}`
     song.play()
+    playButton.textContent = `⏸`
   }
+  //Display current song info
+  currentSongInfo()
 })
 
-    
